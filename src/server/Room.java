639dbe95219c0ea3e4e32513d475f1d4befc0cf2
macarroncio <adrianromeromoreno@gmail.com>
2024-03-04 21:19:54 +0100

    Creacion de las rooms y de los threads de lectura y escritura en el cliente

--- src/server/Room.java
+++ src/server/Room.java
@@ -5,6 +5,57 @@
  */
 package server;
 
-public class Room {
+import java.util.List;
 
+public class Room {
+	private List<String> chat;
+	private String password;
+	private String id;
+	private String name;
+	private boolean isProtected;
+	
+	public Room (String name, String password, String id) {
+		this.name = name;
+		this.password = password;
+		this.id = id;
+		this.isProtected = true;
+	}
+	
+	public Room (String name, String id) {
+		this.name = name;
+		this.id = id;
+		this.isProtected = false;
+	}
+
+	public List<String> getChat() {
+		return chat;
+	}
+	
+	public void addMessage(String message) {//Method used to add a new message to the chat
+		this.chat.add(message);
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	
+	public String getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public boolean getIsProtected() {
+		return this.isProtected;
+	}
+
+	
 }
